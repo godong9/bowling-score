51a2451e602eaf10160a912d0670ae90007aedaf
GoDonghyun <godong9@gmail.com>
2018-02-28 14:00:09 +0900

    점수 관련 수정

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const moment = require("moment");
+const _ = require("lodash");
 
 const UserController = require("./users");
 const FileController = require("./files");
 const ScoreController = require("./scores");
+const UserService = require("../services/users");
+const ScoreService = require("../services/scores");
 
 const router = express.Router();
 
@@ -22,8 +25,35 @@ router.get("/", (req, res) => {
 });
 
 /* GET score page */
-router.get("/score", (req, res) => {
-  res.render("score", {today: moment().format("YYYY-MM-DD")});
+router.get("/score/:nickname", (req, res, next) => {
+  const nickname = req.params.nickname;
+  UserService.getUserByNickname(nickname)
+    .then(user => {
+      if (!user) {
+        return next("Not exist user");
+      }
+      return ScoreService.getScoresByUserId(user.id);
+    })
+    .then(scoreList => {
+      const groupedScoreList = _.chain(scoreList)
+        .groupBy("targetDate")
+        .orderBy(["targetDate", "createdAt"], ['desc', 'desc'])
+        .map(score => {
+          return {
+            targetDate: score.targetDate,
+            score: score.score,
+          }
+        })
+        .value();
+
+      console.log(groupedScoreList);
+      res.render("score", {
+        nickname: req.params.nickname,
+        scoreList: groupedScoreList,
+        today: moment().format("YYYY-MM-DD"),
+      });
+    })
+    .catch(e => next(e));
 });
 
 module.exports = router;
